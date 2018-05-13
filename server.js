162d0b7e590d096001a0d194ee3e7b2d7e656420
Glitch (file-metadata-fcc-faiza) <none>
2018-05-13 05:22:45 +0000

    🙆🐏 Checkpoint ./server.js:1075162/1449

--- server.js
+++ server.js
@@ -4,62 +4,16 @@ var express = require('express');
 var multer = require('multer');
 var bodyParser = require('body-parser');
 var app = express();
-
-const multerConfig = {
-    
-storage: multer.diskStorage({
- //Setup where the user's file will go
- destination: function(req, file, next){
-   next(null, __dirname + '/photo-storage');
-   },   
-    
-    //Then give the file a unique name
-    filename: function(req, file, next){
-        console.log(file);
-        const ext = file.mimetype.split('/')[1];
-        next(null, file.fieldname + '-' + Date.now() + file.size + '.'+ext);
-      }
-    }),   
-    
-    //A means of ensuring only images are uploaded. 
-    fileFilter: function(req, file, next){
-          if(!file){
-            next();
-          }
-        const image = file.mimetype.startsWith('image/');
-        if(image){
-          console.log('photo uploaded');
-          next(null, true);
-        }else{
-          console.log("file not supported");
-          
-          //TODO:  A better message response to user on failure.
-          return next();
-        }     
-  
-    },
-      fileSize: function(req,file,next){
-          if(!file){
-            next();
-          }
-        const size = file.size;
-        console.log(size);
-        next(null, file);
-      }
-  };
-
+var upload = multer({ dest: 'photo/' });
 
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-app.post('/upload',multer(multerConfig).single('photo'),function(req,res){
-   res.send(req.file.size);
+app.post('/upload',upload.single('photo'),function(req,res){
+   res.json({"size": req.file.size});
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
